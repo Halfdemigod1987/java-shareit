a0b1aca71153063c9f1cfc5f13ef41b50e767936
Halfdemigod1987 <theandreyfirst@yandex.ru>
2022-12-24 23:35:53 +0300

    fix: fix test error

--- src/test/java/ru/practicum/shareit/user/services/impl/UserServiceImplIntegrationTest.java
+++ src/test/java/ru/practicum/shareit/user/services/impl/UserServiceImplIntegrationTest.java
@@ -27,23 +27,23 @@ class UserServiceImplIntegrationTest {
 
     @Test
     void findAllUsers_whenInvoked_thenReturnUserCollection() {
-        userRepository.save(User.builder()
+        User user1 = userRepository.save(User.builder()
                 .name("email1@test.com")
                 .email("name1")
                 .build());
-        userRepository.save(User.builder()
+        User user2 = userRepository.save(User.builder()
                 .name("email2@test.com")
                 .email("name2")
                 .build());
-        userRepository.save(User.builder()
+        User user3 = userRepository.save(User.builder()
                 .name("email3@test.com")
                 .email("name3")
                 .build());
 
         List<UserDto> users = userService.findAllUsers();
         assertThat(users, hasSize(3));
-        assertThat(users, hasItem(hasProperty("id", is(1))));
-        assertThat(users, hasItem(hasProperty("id", is(2))));
-        assertThat(users, hasItem(hasProperty("id", is(3))));
+        assertThat(users, hasItem(hasProperty("id", is(user1.getId()))));
+        assertThat(users, hasItem(hasProperty("id", is(user2.getId()))));
+        assertThat(users, hasItem(hasProperty("id", is(user3.getId()))));
     }
 }
